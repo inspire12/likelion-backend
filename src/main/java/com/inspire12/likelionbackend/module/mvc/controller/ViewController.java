ebf4bf3245ac9612eeee59df662a32110eb33fd2
inspire12 <ox4443@naver.com>
2025-03-29 18:01:44 +0900

    feat(core-view-practice): 함정추가

--- src/main/java/com/inspire12/likelionbackend/module/mvc/controller/ViewController.java
+++ src/main/java/com/inspire12/likelionbackend/module/mvc/controller/ViewController.java
@@ -5,9 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+
 @RequestMapping("/view")
+@Controller
+@ResponseBody
 public class ViewController {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
