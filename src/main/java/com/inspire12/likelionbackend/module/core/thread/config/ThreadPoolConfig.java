138362396b6bdaaf9ae1018058d650c72b46db61
inspire12 <ox4443@naver.com>
2025-03-31 10:56:14 +0900

    feat(core-completablefuture): thread pool 대표 등록

--- src/main/java/com/inspire12/likelionbackend/module/core/thread/config/ThreadPoolConfig.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/thread/config/ThreadPoolConfig.java
@@ -1,20 +1,24 @@
 package com.inspire12.likelionbackend.module.core.thread.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
-//@EnableAsync
-//@Configuration
+@EnableAsync
+@Configuration
 public class ThreadPoolConfig {
     private static final int CORE_POOL_SIZE = 2;
 //    private static final int CORE_POOL_SIZE = 10;
     private static final int MAX_POOL_SIZE = 2;
 //    private static final int MAX_POOL_SIZE = 10;
 
-
-//    @Bean(name = "customThreadPoolExecutor")
+    @Primary
+    @Bean(name = "customThreadPoolExecutor")
     public Executor customThreadPoolExecutor() {
         ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
 
