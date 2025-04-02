319f8c114365bb639ccea5bea9c532068854d79a
inspire12 <ox4443@naver.com>
2025-04-02 12:26:05 +0900

    feat(aop-aspect-practice-live): solve

--- src/main/java/com/inspire12/likelionbackend/module/core/aop/aspect/LogExecutionTimeAspect.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/aop/aspect/LogExecutionTimeAspect.java
@@ -5,25 +5,29 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-
+@Order(2)
 @Aspect
 @Component
 public class LogExecutionTimeAspect {
     private static final Logger log = LoggerFactory.getLogger("LogExecutionTimeAspect");
+
     @Around("@annotation(com.inspire12.likelionbackend.module.core.aop.aspect.LogExecutionTime)")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object logExecutionTime
+            (ProceedingJoinPoint joinPoint) throws Throwable {
         /*TODO
         * Aspect 내에 어디에 본 함수를 실행할까? joinPoint.proceed() 위치 찾기
         * */
         /* 여기는 함수의 시그니처에 맞춰서 작성해야한다 */
-        Object[] args = joinPoint.getArgs();
-        String username = (args.length > 0 && args[0] instanceof String) ?  (String) args[0]: "unknown";
+        String username = joinPoint.getArgs()[0].toString();
+//        String email = joinPoint.getArgs()[1].toString();
         log.info("사용자 등록 시작 - username: {}", username);
         long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
         long endTime = System.currentTimeMillis();
         log.info("사용자 등록 완료 - username: {} {}", username, endTime - startTime);
-        return proceed;
+        return result;
     }
 }
--- src/main/java/com/inspire12/likelionbackend/module/core/aop/aspect/UserInputValidateAspect.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/aop/aspect/UserInputValidateAspect.java
@@ -4,12 +4,16 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(1)
 @Aspect
 @Component
 public class UserInputValidateAspect {
-//    private static final Logger log = LoggerFactory.getLogger("UserInputValidateAspect");
+    private static final Logger log = LoggerFactory.getLogger("UserInputValidateAspect");
 
     @Pointcut("@annotation(com.inspire12.likelionbackend.module.core.aop.aspect.UserInputValidate)")
     public void userInputValidatePointcut() {}
@@ -17,6 +21,17 @@ public class UserInputValidateAspect {
     @Around("userInputValidatePointcut()")
     public Object userInputValidate(ProceedingJoinPoint joinPoint) throws Throwable {
         /* TODO username, email 두 input 값을 확인하는 코드와 함수 실행시점인 jointPoint.proceed() 넣어주세요 */
-
+        String username = joinPoint.getArgs()[0].toString();
+        String email = joinPoint.getArgs()[1].toString();
+        // 유효성 검사
+        if (username == null || email == null) {
+            throw new IllegalArgumentException("입력값이 비어 있습니다.");
+        }
+        // 이메일 검증
+        if (!email.endsWith("@example.com")) {
+            throw new SecurityException("허용되지 않은 이메일 도메인입니다.");
+        }
+        log.info("검증 완료");
+        return joinPoint.proceed();
     }
 }
