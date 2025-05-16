e93b90aba8b3efc51f19ea860f1aa255dda915dd
inspire12 <ox4443@naver.com>
2025-05-16 10:12:15 +0900

    feat(monitor): 버그픽스

--- src/main/java/com/inspire12/likelionbackend/config/LogConfig.java
+++ src/main/java/com/inspire12/likelionbackend/config/LogConfig.java
@@ -1,4 +1,12 @@
 package com.inspire12.likelionbackend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class LogConfig {
+//    @Bean
+//    public OpenTelemetryAgentSpanContextSupplier openTelemetryAgentSpanContextSupplier() {
+//        return new OpenTelemetryAgentSpanContextSupplier();
+//    }
 }
